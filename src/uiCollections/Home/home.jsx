c62d917143e42dc534279a2de2ec32f9942e54e5
Ishita <goswami.ishita13@gmail.com>
2024-02-09 18:17:20 +0530

    Add Token Autentication button

--- src/uiCollections/Home/home.jsx
+++ src/uiCollections/Home/home.jsx
@@ -1,25 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
-import Toast from '../toast/toast';
-
+import Toast from '../Toast/Toast';
+import {useNavigate}  from 'react-router-dom' ;
+import axios from 'axios';
 const Main = () => {
-    const [showToast, setShowToast] = useState(false);
     const location = useLocation();
     const name = location.state.props;
+    const [showToast, setShowToast] = useState(false);
+    const navigate = useNavigate(); 
+   
     console.log(`My name ${name}`)
 
     useEffect(() => {
         setShowToast(true)
         setTimeout(() => {
             setShowToast(false);
-        }, 5000);
+        }, 2000);
     }, []);
 
-
+    const handleData = ()=>{
+        axios.get('http://localhost:8000/home' ,{withCredentials: true })
+      .then(response => {
+        console.log("respose",response)
+        console.log(response.data)
+        if(response.data === "Token verification failed" || response.data ==="User not authenticated"  ){
+            document.getElementById('userdata').innerText = `${response.data}`;
+            
+            navigate("/login");
+          
+          
+        }else{
+          
+          document.getElementById('userdata').innerText = `${response.data}`;
+        }
+        
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+      }
+      
     return (
         <>
-            <h1 className="home">Hey, {name}</h1>
+            <h1 className="home">Hello {name}</h1>
+            <button className="auth" onClick={handleData}>GETDATA</button>
+            <div id='userdata'></div>
             {showToast && <Toast message={"Login successful"}/>}
+
         </>
     );
 };
